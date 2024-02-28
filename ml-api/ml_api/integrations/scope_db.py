74c05da7a6453c71c7e9f81710cb0a28e958021c
Michael-Wojdak <mjwojdak@email.wm.edu>
2024-02-28 18:30:24 -0500

    initial connection to database

--- ml-api/ml_api/integrations/scope_db.py
+++ ml-api/ml_api/integrations/scope_db.py
@@ -2,13 +2,27 @@
 
 import logging
 
+from MySQLdb import _mysql
+from MySQLdb.constants import FIELD_TYPE
+
 logger = logging.getLogger(__name__)
+
+my_conv = { FIELD_TYPE.LONG: int }
 
 
 def get_document(document_id: str) -> dict:
     """Get a document from the database."""
+    # Should a new connection made everytime the function is called?
+    # Proper path to config file?
+    db=_mysql.connect(conv=my_conv, read_default_file="db_config.cnf")
+    # TODO logic for getting url from id
+    db.query("""SELECT * FROM scopesql.scopeBackend_source""")
+    r = db.store_result()
+
+    data = r.fetch_row(maxrows=1)
     return {
         "id": document_id,
+        "data": data,
         "text": "This is the text of the document.",
     }
 
@@ -22,3 +36,6 @@ def _get_sample_document(document_id: str = "sample_id") -> dict:
         "id": document_id,
         "text": sample_text,
     }
+
+
+print(get_document("id")["data"])
