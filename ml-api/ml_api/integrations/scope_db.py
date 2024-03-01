6fac759c05bae337375dcf465bb86099838ea18e
Michael-Wojdak <mjwojdak@email.wm.edu>
2024-03-01 15:25:48 -0500

    improved connection logic

--- ml-api/ml_api/integrations/scope_db.py
+++ ml-api/ml_api/integrations/scope_db.py
@@ -2,27 +2,40 @@
 
 import logging
 
-from MySQLdb import _mysql
+import MySQLdb
 from MySQLdb.constants import FIELD_TYPE
 
 logger = logging.getLogger(__name__)
 
-my_conv = { FIELD_TYPE.LONG: int }
-
 
 def get_document(document_id: str) -> dict:
     """Get a document from the database."""
-    # Should a new connection made everytime the function is called?
-    # Proper path to config file?
-    db=_mysql.connect(conv=my_conv, read_default_file="db_config.cnf")
-    # TODO logic for getting url from id
-    db.query("""SELECT * FROM scopesql.scopeBackend_source""")
-    r = db.store_result()
 
-    data = r.fetch_row(maxrows=1)
+    # Specifies what types certain fields in the database should be converted to (not currently needed)
+    my_conv = { FIELD_TYPE.LONG: int }
+
+    # Should a new connection made everytime the function is called?
+    db=MySQLdb.connect(conv=my_conv, read_default_file="db_config.cnf")
+
+    c=db.cursor()
+    # Security (could any string be harmful here?)
+    c.execute("""SELECT url FROM scopesql.scopeBackend_source WHERE id = %s""", (document_id,))
+    
+    # fetchone() returns a 1-tuple containing the url
+    document_url = c.fetchone()
+    if document_url is None:
+        document_url = ""
+    else:
+        document_url = document_url[0]
+
+    c.close()
+    db.close()
+
+    # pass document_url to text extractor
+
     return {
         "id": document_id,
-        "data": data,
+        "url": document_url,
         "text": "This is the text of the document.",
     }
 
@@ -36,6 +49,3 @@ def _get_sample_document(document_id: str = "sample_id") -> dict:
         "id": document_id,
         "text": sample_text,
     }
-
-
-print(get_document("id")["data"])
