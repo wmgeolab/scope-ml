2df103304813b3052b9e3726062fa0f2393e2d19
Sawyer <sawyer@teamculture.ai>
2024-03-26 14:13:25 -0400

    Add function to retrieve a sourcing source by ID

--- ml-api/ml_api/scope_db/crud.py
+++ ml-api/ml_api/scope_db/crud.py
@@ -1,7 +1,7 @@
 from sqlmodel import Session, select
 
 from .database import engine
-from .models import ScopeSource
+from .models import ScopeSource, SourcingSource
 
 
 def get_document(document_id: int) -> ScopeSource:
@@ -11,3 +11,11 @@ def get_document(document_id: int) -> ScopeSource:
         document = session.exec(statement).one()
 
         return document
+
+
+def get_sourcing_source(source_id: int) -> SourcingSource:
+    with Session(engine) as session:
+        statement = select(SourcingSource).where(SourcingSource.id == source_id)
+        source = session.exec(statement).one()
+
+        return source
