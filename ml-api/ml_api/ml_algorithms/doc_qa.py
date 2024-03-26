394f18fd2c72e325caee1a130834c70d6f3c7f42
Sawyer <sawyer@teamculture.ai>
2024-03-26 14:14:09 -0400

    Add location extraction functionality

--- ml-api/ml_api/ml_algorithms/doc_qa.py
+++ ml-api/ml_api/ml_algorithms/doc_qa.py
@@ -6,6 +6,9 @@ import logging
 from llama_index.llms.together import TogetherLLM
 from ..scope_db.crud import get_document
 
+from ..models.extraction import ExtractedActors, ExtractedLocations
+from ..prompts import EXTRACT_ACTORS_TEMPLATE, EXTRACT_LOCATIONS_TEMPLATE
+
 logger = logging.getLogger(__name__)
 
 
@@ -21,9 +24,18 @@ def answer_question(document_id: int, question: str) -> str:
     )
 
 
-def extract_locations(document_id: str):
-    pass
+def extract_locations(document_id: int) -> ExtractedLocations:
+
+    document = get_document(document_id)
+
+    response = llm.structured_predict(
+        ExtractedLocations, EXTRACT_LOCATIONS_TEMPLATE, document_text=document.text  # type: ignore
+    )
+
+    valid_response = ExtractedLocations.model_validate(response)
+
+    return valid_response
 
 
-def extract_actors(document_id: str):
+def extract_actors(document_id: int):
     pass
