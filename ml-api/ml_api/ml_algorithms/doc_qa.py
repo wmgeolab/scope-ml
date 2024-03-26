f88263dd60773059915ec6f4c5c0246c45b2fd98
Sawyer <sawyer@teamculture.ai>
2024-03-26 15:08:59 -0400

    Refactor code to use sourcing source instead of document

--- ml-api/ml_api/ml_algorithms/doc_qa.py
+++ ml-api/ml_api/ml_algorithms/doc_qa.py
@@ -4,7 +4,7 @@ This file is for interacting with models to answer questions about documents.
 
 import logging
 from llama_index.llms.together import TogetherLLM
-from ..scope_db.crud import get_document
+from ..scope_db.crud import get_document, get_sourcing_source
 
 from ..models.extraction import ExtractedActors, ExtractedLocations
 from ..prompts import EXTRACT_ACTORS_TEMPLATE, EXTRACT_LOCATIONS_TEMPLATE
@@ -17,7 +17,8 @@ llm = TogetherLLM(model="mistralai/Mixtral-8x7B-Instruct-v0.1")
 
 def answer_question(document_id: int, question: str) -> str:
     """Answer a question about a document."""
-    document = get_document(document_id)
+    # document = get_document(document_id)
+    source = get_sourcing_source(document_id)
 
     return (
         f"Answering the question '{question}' about the document with id {document_id}."
@@ -26,10 +27,11 @@ def answer_question(document_id: int, question: str) -> str:
 
 def extract_locations(document_id: int) -> ExtractedLocations:
 
-    document = get_document(document_id)
+    # document = get_document(document_id)
+    source = get_sourcing_source(document_id)
 
     response = llm.structured_predict(
-        ExtractedLocations, EXTRACT_LOCATIONS_TEMPLATE, document_text=document.text  # type: ignore
+        ExtractedLocations, EXTRACT_LOCATIONS_TEMPLATE, document_text=source.source_text  # type: ignore this is because the library is using pydantic v1
     )
 
     valid_response = ExtractedLocations.model_validate(response)
