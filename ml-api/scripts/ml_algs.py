4cdbb78a38773e00ca59cca2d3d9715eb62e00e8
Sawyer Czupka <sawyerczupka@gmail.com>
2024-04-10 16:29:27 -0400

    Add CSV export functionality for extracted locations and actors

--- ml-api/scripts/ml_algs.py
+++ ml-api/scripts/ml_algs.py
@@ -1,28 +1,33 @@
+import csv
 import logging
+import os
 
 from ml_api.ml_algorithms.extract import doc_extract_actors, doc_extract_locations
-from ml_api.models.extraction import ExtractedActors, ExtractedLocations
+from ml_api.models.extraction import (
+    Actor,
+    ExtractedActors,
+    ExtractedLocations,
+    Location,
+)
 from ml_api.scope_db.crud import get_document, get_sourcing_source, get_sourcing_sources
 
 logging.basicConfig(level=logging.INFO)
 
 
-def extract_locations_test(doc_id: int = 2):
+def extract_locations_test(doc_id: int = 2) -> ExtractedLocations:
     extracted_locations = doc_extract_locations(doc_id)
 
-    for location in extracted_locations.locations:
-        print(location)
+    return extracted_locations
 
 
-def extract_actors_test(doc_id: int = 2):
+def extract_actors_test(doc_id: int = 2) -> ExtractedActors:
     extracted_actors = doc_extract_actors(doc_id)
 
-    for actor in extracted_actors.actors:
-        print(actor)
+    return extracted_actors
 
 
 def get_sourcing_sources_test():
-    sources = get_sourcing_sources()
+    return get_sourcing_sources()
 
 
 def get_sourcing_source_test(doc_id: int = 1):
@@ -33,8 +38,27 @@ def get_sourcing_source_test(doc_id: int = 1):
 
 
 def main():
-    extract_locations_test()
-    # get_sourcing_sources_test()
+    locs = extract_locations_test()
+    acts = extract_actors_test()
+
+    # add to csv sample_locations.csv and sample_actors.csv in current dir
+    # This should append to the csv if it already exists
+
+    with open("sample_locations.csv", "a", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=Location.__fields__.keys())
+        # If the file is empty, write the header
+        if os.stat("sample_locations.csv").st_size == 0:
+            writer.writeheader()
+        for location in locs.locations:
+            writer.writerow(location.dict())
+
+    with open("sample_actors.csv", "a", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=Actor.__fields__.keys())
+        # If the file is empty, write the header
+        if os.stat("sample_actors.csv").st_size == 0:
+            writer.writeheader()
+        for actor in acts.actors:
+            writer.writerow(actor.dict())
 
 
 if __name__ == "__main__":
