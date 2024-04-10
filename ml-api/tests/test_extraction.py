21bc3fe2e52aca87e3398c739a65b5705b0be2a6
Sawyer <sawyer@teamculture.ai>
2024-04-10 11:20:28 -0400

    Add assertions to test_extraction.py and test_summarize.py for empty responses

--- ml-api/tests/test_extraction.py
+++ ml-api/tests/test_extraction.py
@@ -10,6 +10,7 @@ def test_extract_locations(document_id: int):
     """Test that the extract_locations function runs without errors and returns an ExtractedLocations object."""
     extracted_locations = doc_extract_locations(document_id)
     assert isinstance(extracted_locations, ExtractedLocations)
+    assert extracted_locations.locations != []
 
 
 @pytest.mark.parametrize("document_id", TestConfig.VALID_DOCUMENT_IDS)
@@ -17,3 +18,4 @@ def test_extract_actors(document_id: int):
     """Test that the extract_actors function runs without errors and returns an ExtractedActors object."""
     extracted_actors = doc_extract_actors(document_id)
     assert isinstance(extracted_actors, ExtractedActors)
+    assert extracted_actors.actors != []
--- ml-api/tests/test_summarize.py
+++ ml-api/tests/test_summarize.py
@@ -10,3 +10,4 @@ def test_generate_summary(document_id: int):
     """Test that the generate_summary function runs without errors and returns a SummarizeDocumentResponse object."""
     generated_summary = doc_generate_summary(document_id)
     assert isinstance(generated_summary, SummarizeDocumentResponse)
+    assert generated_summary.summary != ""
