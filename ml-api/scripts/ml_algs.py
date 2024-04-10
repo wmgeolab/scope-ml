fb2ed0966aa4f410fe08ec9f1fb1cc0ac7bf1e96
Sawyer <sawyer@teamculture.ai>
2024-04-10 12:43:49 -0400

    Refactor ml_algs.py to improve code readability and maintainability

--- ml-api/scripts/ml_algs.py
+++ ml-api/scripts/ml_algs.py
@@ -7,19 +7,26 @@ from ml_api.scope_db.crud import get_document, get_sourcing_source, get_sourcing
 logging.basicConfig(level=logging.INFO)
 
 
-def extract_locations_test():
-    extracted_locations = doc_extract_locations(2)
+def extract_locations_test(doc_id: int = 2):
+    extracted_locations = doc_extract_locations(doc_id)
 
     for location in extracted_locations.locations:
         print(location)
+
+
+def extract_actors_test(doc_id: int = 2):
+    extracted_actors = doc_extract_actors(doc_id)
+
+    for actor in extracted_actors.actors:
+        print(actor)
 
 
 def get_sourcing_sources_test():
     sources = get_sourcing_sources()
 
 
-def get_sourcing_source_test():
-    source = get_sourcing_source(1)
+def get_sourcing_source_test(doc_id: int = 1):
+    source = get_sourcing_source(doc_id)
     print(type(source))
     # print(source.source_text)
     print(source.source_date)
